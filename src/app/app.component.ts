fa51eb27edc4ab332d265e6d3cc08436dca5d95a
webdatavisualdev <cli698638@gmail.com>
2019-10-29 13:23:17 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './service/api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather';
+  cities = ['London', 'NY', 'Tokyo'];
+  city = '';
+  weather: any;
+  dice = 1;
+  dices = [];
+
+  constructor(private api: ApiService) {}
+
+  changeCity() {
+    if (this.city) {
+      this.api.getWeather().subscribe((res: any) => {
+        this.weather = res[this.city].sort((a, b) => {
+          return new Date(b.date).getTime() - new Date(a.date).getTime();
+        })[0];
+      });
+    }
+  }
+
+  rollDice() {
+    this.dice = Math.floor(Math.random() * 7) || 1;
+  }
+
+  testCase() {
+    this.dices = [];
+    for (let i = 0 ; i < 1000 ; i ++) {
+      this.rollDice();
+      this.dices.push(this.dice);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from './service/api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ApiService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
